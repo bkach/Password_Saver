9a9955020f7a3ba4621a630859b5c0d2033bfafc
Boris Kachscovsky <kachscovsky@gmail.com>
2013-02-13 11:48:14 -0500

    Initial Commit

---
+++ password_saver/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  password_saver
+//
+//  Created by Boris Kachscovsky on 2/13/13.
+//  Copyright (c) 2013 Boris Kachscovsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
